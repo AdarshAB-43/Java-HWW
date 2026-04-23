4c4da515511d61c5357911dcf9504a6e7ddb8b9d
Adarsh <adreshtakudu@gmail.com>
2026-04-23 22:24:58 +0530

    commit

---
+++ billingtool.java
@@ -0,0 +1,12 @@
+public class billingtool {
+       public static void main(String[] args) {
+    String item="Soap";
+    int quantity = 4;
+    float unitprice = 18.75f;
+    float totalprice = quantity * unitprice;
+    System.out.println("Item: " + item);
+    System.out.println("Quantity: " + quantity);
+    System.out.println("Unit Price: " + unitprice);
+    System.out.println("Total Cost:" + totalprice);
+}
+}
