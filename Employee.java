12eea43d7c08293480d8acbeb752e5c25a4b7f1c
Adarsh AB <adreshtakudu@gmail.com>
2026-05-11 11:12:58 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,1 +1,47 @@
 
+// Employee.java
+class Employee {
+    // Private fields (Encapsulation)
+    private String name;
+    private int idNumber;
+    private String department;
+    private double salary;
+
+    // Getter and Setter methods
+
+    // Name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // ID Number
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    // Department
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    // Salary
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
